279fd60e979620d95e79a0e91e8b73a8f0894bd3
Omar <omar.branez@gmail.com>
2021-11-01 13:39:20 -0400

    arrange breed cards in grid

--- src/components/breeds/breed.js
+++ src/components/breeds/breed.js
@@ -1,11 +1,106 @@
-import React from 'react'
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props
+  return <IconButton {...other} />
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}))
 
 const Breed = (props) => {
-    return(
-        <div>
-            <p>{props.breed}</p>
-        </div>
-    )
+  const [expanded, setExpanded] = React.useState(false)
+  console.log(props.reportData)
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {props.breed[0]}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.breed}
+      />
+      {/* <CardMedia
+        component="img"
+        height="194"
+        // image="/static/images/cards/paella.jpg"
+        alt={props.breed_group + " Group"}
+      /> */}
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <CardContent>
+          
+        <Typography variant="body2" color="text.secondary">
+          {props.temperament}
+        </Typography>
+      </CardContent>
+        <CardContent>
+          <Typography paragraph>Statistics:</Typography>
+          {props.reportData ? <Typography paragraph>There have been {props.reportData.length} reports of this breed.</Typography> : null}
+
+          <Typography paragraph>Size:</Typography>
+          <Typography paragraph>
+            Height: {props.height} inches
+          </Typography>
+          <Typography paragraph>
+            Weight: {props.weight} lbs
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  )
 }
 
 export default Breed
--- src/containers/BreedsContainer.js
+++ src/containers/BreedsContainer.js
@@ -1,25 +1,44 @@
 import React, {useEffect} from 'react'
 import { connect, useDispatch} from 'react-redux'
 import { getBreeds } from '../actions/breeds'
+import { getReports } from '../actions/reports'
 import Breed from '../components/breeds/breed'
 import Modal from 'react-modal'
+import Grid from '@mui/material/Grid'
 
 const BreedsContainer = (props) => {
+
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(getBreeds())
     }, [dispatch])
 
+    useEffect(() => {
+        dispatch(getReports())
+    }, [dispatch])
+
+    const filteredReports = (breed) => {
+       return props.reports.filter(report => report.breed === breed.breed)
+    }
+    
+    // console.log(filteredReports("Pembroke Welsh Corgi"))
+    // console.log(props.reports.filter(report => report.breed === "Pembroke Welsh Corgi"))
+    // debugger
     return(
         <div>
             <h2>Current List of Breeds</h2>
-            {props.breeds.map((breed) => <Breed key={props.dog_id} {...breed}/> )}
+            <Grid container spacing={3}>
+            {props.breeds.map((breed) => <Grid item xs={3}><Breed key={props.dog_id} {...breed} reportData={filteredReports(breed)}/> </Grid>)}
+            {/* insert reports for statistics (how many of this breed, etc) */}
+            </Grid>
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    breeds: state.breeds.breeds
+    breeds: state.breeds.breeds,
+    reports: state.reports.reports
 })
 
-export default connect(mapStateToProps, {getBreeds})(BreedsContainer)
+export default connect(mapStateToProps, {getBreeds, getReports})(BreedsContainer)
